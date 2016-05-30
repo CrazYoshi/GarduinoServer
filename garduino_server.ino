d0c0cbb01b0cb03623ff2dc2dd74c094846c34fd
CrazYoshi <mar862002@hotmail.com>
2016-05-30 08:09:53 +0200

    Tabulation

--- garduino_server.ino
+++ garduino_server.ino
@@ -39,26 +39,26 @@ void setup() {
   server.begin();
 
   dht.begin();
-  if(!bmp.begin())// Initialise the sensor
+  if (!bmp.begin()) // Initialise the sensor
   {
-    Serial.print("No BMP085 detected ... Check your wiring or I2C ADDR!");/* There was a problem detecting the BMP085 ... check your connections */
+    Serial.print("No BMP085 detected!");/* There was a problem detecting the BMP085 ... check your connections */
   }
-  
-  pinMode(S1,OUTPUT);           //Multiplexer initializing
-  pinMode(S2,OUTPUT); 
-  pinMode(S3,OUTPUT); 
-  
+
+  pinMode(S1, OUTPUT);          //Multiplexer initializing
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+
   pinMode( UStrigger, OUTPUT ); //HC SR04
   pinMode( USecho, INPUT );
-  
-  pinMode(PUMP1,OUTPUT);        //Pump setup
-  pinMode(PUMP2,OUTPUT);  
-  pinMode(PUMP3,OUTPUT);
-  digitalWrite(PUMP1,HIGH);
-  digitalWrite(PUMP2,HIGH);
-  digitalWrite(PUMP3,HIGH);
 
-  pinMode(TRANSISTOR,OUTPUT); // Moisture transistor s
+  pinMode(PUMP1, OUTPUT);       //Pump setup
+  pinMode(PUMP2, OUTPUT);
+  pinMode(PUMP3, OUTPUT);
+  digitalWrite(PUMP1, HIGH);
+  digitalWrite(PUMP2, HIGH);
+  digitalWrite(PUMP3, HIGH);
+
+  pinMode(TRANSISTOR, OUTPUT); // Moisture transistor s
   digitalWrite(TRANSISTOR, HIGH);  // Disable sensors reading
 }
 
@@ -72,117 +72,113 @@ void loop() {
   delay(200);                         // Poll every 200ms
 }
 
-void process(BridgeClient client){
+void process(BridgeClient client) {
   DynamicJsonBuffer jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
   JsonArray& data = root.createNestedArray("moisture");
   String command = client.readStringUntil('/');
   command.trim();
-  
-  Serial.print("Command received: ");
-  Serial.println(command);
 
-  if(command == "startPump"){
+  Serial.print("Command received: "); Serial.println(command);
+
+  if (command == "startPump") {
     int pumpNum = client.parseInt();
-    Serial.print("Pump number selected: ");
-    Serial.println(pumpNum);
+    Serial.print("Pump number selected: "); Serial.println(pumpNum);
 
     startPump(pumpNum);
   }
 
-  if(command == "stopPump"){
+  if (command == "stopPump") {
     int pumpNum = client.parseInt();
-    Serial.print("Pump number selected: ");
-    Serial.println(pumpNum);
+    Serial.print("Pump number selected: "); Serial.println(pumpNum);
 
     stopPump(pumpNum);
   }
-  
+
   root["temperature"] = dht.readTemperature();
   root["humidity"] = dht.readHumidity();
   root["light"] = Lumen(analogRead(LIGHT));
   root["pressure"] = getPressure();
   root["water"] = getWaterLevel();
   getMoisture(data);
-  
+
   root.printTo(client);
 }
 
-float getPressure(){
+float getPressure() {
   sensors_event_t event;
   bmp.getEvent(&event);
-  return event.pressure;
+  if (event.pressure)
+  {
+    Serial.print("Pressure:    "); Serial.print(event.pressure); Serial.println(" hPa");
+    return event.pressure;
+  }
+  return null;
 }
 
-long getWaterLevel(){
+long getWaterLevel() {
   digitalWrite( UStrigger, LOW );   // porta bassa l'uscita del trigger
   digitalWrite( UStrigger, HIGH );  // invia un impulso di 10microsec su trigger
   delayMicroseconds( 10 );
   digitalWrite( UStrigger, LOW );
-   
+
   long duration = pulseIn( USecho, HIGH );
   long r;
-  if(duration > 38000 ) r = -1;
+  if (duration > 38000 ) r = -1;
   else r = 0.034 * duration / 2;
-  return r;  
+  return r;
 }
 
 int Lumen(int lightSensorValue)
 {
-  Serial.print("Light sensor value: ");
-  Serial.println(lightSensorValue);
-  
-  float Res0=10.0;  // Resistance in the circuit of sensor 0 (KOhms)
-  float Vout0=lightSensorValue*0.0048828125;
-  return 500/(Res0*((5-Vout0)/Vout0));
+  Serial.print("Light sensor value: "); Serial.println(lightSensorValue);
+
+  float Res0 = 10.0; // Resistance in the circuit of sensor 0 (KOhms)
+  float Vout0 = lightSensorValue * 0.0048828125;
+  return 500 / (Res0 * ((5 - Vout0) / Vout0));
 }
 
 // MOISTURE function
-void getMoisture(JsonArray& array){
+void getMoisture(JsonArray& array) {
   int r0 = 0, r1 = 0, r2 = 0;
-  
+
   digitalWrite(TRANSISTOR, LOW); // Enable sensors reading
 
-  for (int count=0; count<=7; count++) {  
-    r0 = bitRead(count,0);    // select the bit   
-    r1 = bitRead(count,1);   
-    r2 = bitRead(count,2);
+  for (int count = 0; count <= 7; count++) {
+    r0 = bitRead(count, 0);   // select the bit
+    r1 = bitRead(count, 1);
+    r2 = bitRead(count, 2);
 
     digitalWrite(S1, r0);
     digitalWrite(S2, r1);
     digitalWrite(S3, r2);
-    
+
     int sensorRead = analogRead(MOISTURE);
     array.add(sensorRead);
-    
-    Serial.print("Moisture sensor ");           // LOG
-    Serial.print(count);
-    Serial.print(" value: ");
-    Serial.println(sensorRead);
+
+    Serial.print("Moisture sensor "); Serial.print(count); Serial.print(" value: "); Serial.println(sensorRead);
   }
-  
+
   digitalWrite(TRANSISTOR, HIGH);  // Disable sensors reading
 }
 
 // PUMP functions
-int getPump(int number){
-  switch(number){
+int getPump(int number) {
+  switch (number) {
     case 1: return PUMP1;     //Get the PIN number from the number given
     case 2: return PUMP2;
     case 3: return PUMP3;
   }
 }
 
-void startPump(int pumpNumber){
-  Serial.print("Starting pump ");
-  Serial.println(pumpNumber);
-  
-  digitalWrite(getPump(pumpNumber),LOW);
+void startPump(int pumpNumber) {
+  Serial.print("Starting pump "); Serial.println(pumpNumber);
+
+  digitalWrite(getPump(pumpNumber), LOW);
 }
 
-void stopPump(int pumpNumber){
-  Serial.print("Stopping pump ");
-  Serial.println(pumpNumber);
-  
-  digitalWrite(getPump(pumpNumber),HIGH);
+void stopPump(int pumpNumber) {
+  Serial.print("Stopping pump "); Serial.println(pumpNumber);
+
+  digitalWrite(getPump(pumpNumber), HIGH);
 }
